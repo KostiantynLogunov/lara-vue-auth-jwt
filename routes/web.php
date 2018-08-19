14559cc8036c37e10ec0e17ed4609faadf06d871
Kostiantyn Logunov <777koc@gmail.com>
2018-08-19 18:47:37 +0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+/*Route::post('/api/auth/register', 'AuthController@register');
+
+Route::post('/api/auth/login', 'AuthController@login');
+Route::group(['middleware' => 'jwt.auth'], function(){
+    Route::get('/api/auth/user', 'AuthController@user');
+    Route::post('/api/auth/logout', 'AuthController@logout');
+});
+Route::group(['middleware' => 'jwt.refresh'], function(){
+    Route::get('/api/auth/refresh', 'AuthController@refresh');
+});*/
+
+
+
